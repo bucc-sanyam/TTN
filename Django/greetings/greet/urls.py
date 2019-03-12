893be63fc68889a62af7faea7f50b4e20540bf1c
Sanyam Gupta <sanyamgupta2307@gmail.com>
2019-03-12 17:49:03 +0530

    Django Project created

---
+++ Django/greetings/greet/urls.py
@@ -0,0 +1,10 @@
+from .views import *
+from django.urls import path
+urlpatterns = [
+    path('home/', home, name="home"),
+    path('daytime/<int:time>/', daytime, name='daytime'),
+    path('file/input/', finput, name='finput'),
+    path('file/output/<str:fileName>', foutput, name="foutput"),
+    path('file/update/<str:fileName>', fupdate, name='fupdate'),
+    path('file/delete/<str:fileName>', delfile, name = 'delfile'),
+]
---
+++ Django/greetings/greet/views.py
@@ -0,0 +1,66 @@
+import datetime
+import os
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+
+def daytime(request, time):
+    """ Function to print various actions """
+
+    if 12 > time >= 6:
+        return HttpResponse("Good Morning!")
+    elif 18 > time >= 12:
+        return HttpResponse("Good Afternoon!")
+    elif 24 > time >= 18:
+        return HttpResponse("Good Night!")
+    else:
+        return HttpResponse("Go to bed!!")
+
+
+def home(request):
+    """ Function to print the time of the day (default timezone set to India) """
+
+    timenow = datetime.datetime.now()
+    if timenow.hour >= 12:
+        return HttpResponse("Good Evening!")
+    else:
+        return HttpResponse("Good Morning!")
+
+
+@csrf_exempt
+def finput(request):
+    if request.method == 'POST':
+        json_data = request.body.decode("utf-8")
+        data = json.loads(json_data)
+        fileName = data['file_name']
+        write_content = data['content']
+
+        with open(fileName + ".txt", "w+") as file:
+            file.write(write_content)
+
+    return HttpResponse("Thanks")
+
+
+def foutput(request, fileName):
+    with open(fileName + ".txt", "r") as file:
+        read_content = file.read()
+    return HttpResponse(read_content)
+
+
+@csrf_exempt
+def fupdate(request, fileName):
+    if request.method == 'POST':
+        json_data = request.body.decode("utf-8")
+        data = json.loads(json_data)
+        write_content = data['content']
+        with open(fileName + ".txt", "a") as file:
+            file.write(write_content)
+
+    return HttpResponse("Updated")
+
+
+def delfile(request, fileName):
+    os.remove(fileName + ".txt")
+    return HttpResponse("File deleted.")
