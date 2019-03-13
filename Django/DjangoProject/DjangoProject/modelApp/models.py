921253ed109c859762c2ab692f114c47565d16f3
Sanyam Gupta <sanyamgupta2307@gmail.com>
2019-03-14 00:27:22 +0530

    Models Exercise

---
+++ Django/DjangoProject/DjangoProject/modelApp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    salary = models.IntegerField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    reporting_manager = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+
+    designation_choices = [
+        ('CEO', 'CEO'),
+        ('Senior Software Engineer', 'Senior Software Engineer'),
+        ('Project Manager', 'Project Manager'),
+        ('Trainee', 'Trainee'),
+    ]
+
+    designation = models.CharField(max_length=50, choices=designation_choices)
